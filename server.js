5be85fbb76ee255cd681ee6a9154a1e94587f186
Konstantin Krumin <konstantin.krumin@gmail.com>
2021-10-29 00:01:22 +0300

    setup the project

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const cors = require('cors');
+
 require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.ENV['MONGO_URI'], {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
